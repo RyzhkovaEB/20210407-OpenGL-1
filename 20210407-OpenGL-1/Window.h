4ea5094ab0759ebabb808336e29c093d950a5cd8
erijkova <liza.ryzhkova74@gmail.com>
2021-04-13 00:32:57 +0500

    Реализован конструктор Window

--- 20210407-OpenGL-1/Window.h
+++ 20210407-OpenGL-1/Window.h
@@ -8,9 +8,36 @@
 #ifndef WINDOW_H_
 #define WINDOW_H_
 
+
+#include <memory>
+#include <SDL2/SDL.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+
 class Window {
+
 public:
+	constexpr int DEFAULT_WIDTH = 1080;
+	constexpr int DEFAULT_HEIGHT = 800;
+	constexpr int DEFAULT_X = SDL_WINDOWPOS_CENTERED;
+	constexpr int DEFAULT_Y = SDL_WINDOWPOS_CENTERED;
+
+protected:
+	std::shared_ptr<SDL_Window> _window;// окно
+	std::shared_ptr<void> _gl_context;// Контекст OpenGL
+	int _width, _height;
+public:
+	Window(int width = DEFAULT_WIDTH, int Height = DEFAULT_HEIGHT);
 	virtual ~Window() = default;
+
+	int getHeight() const {
+		return _height;
+	}
+
+	int getWidth() const {
+		return _width;
+	}
 };
 
 #endif /* WINDOW_H_ */
