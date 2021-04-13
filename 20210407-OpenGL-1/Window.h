0b9fcb873961535f51ba0057afb793b3c17f3abb
erijkova <liza.ryzhkova74@gmail.com>
2021-04-13 15:45:20 +0500

    Готов верх и низ куба, конец Лаб Раб

--- 20210407-OpenGL-1/Window.h
+++ 20210407-OpenGL-1/Window.h
@@ -1,43 +1,48 @@
 /*
  * Window.h
  *
- *  Created on: 12 апр. 2021 г.
- *      Author: erijkova
+ *  Created on: 7 апр. 2021 г.
+ *      Author: unyuu
  */
 
 #ifndef WINDOW_H_
 #define WINDOW_H_
-
 
 #include <memory>
 #include <SDL2/SDL.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 
+class Window
+{
+public:
+	static constexpr int DEFAULT_WIDTH = 1000;
+	static constexpr int DEFAULT_HEIGHT = 800;
+	static constexpr int DEFAULT_X = SDL_WINDOWPOS_CENTERED_DISPLAY(1);
+	static constexpr int DEFAULT_Y = SDL_WINDOWPOS_CENTERED_DISPLAY(1);
 
-class Window {
+private:
+	std::shared_ptr<SDL_Window> _window; // Окно
+	std::shared_ptr<void> _gl_context; // Контекст OpenGL
+	int _width, _height; //Ширина и высота окна
 
 public:
-	constexpr int DEFAULT_WIDTH = 1080;
-	constexpr int DEFAULT_HEIGHT = 800;
-	constexpr int DEFAULT_X = SDL_WINDOWPOS_CENTERED;
-	constexpr int DEFAULT_Y = SDL_WINDOWPOS_CENTERED;
-
-protected:
-	std::shared_ptr<SDL_Window> _window;// окно
-	std::shared_ptr<void> _gl_context;// Контекст OpenGL
-	int _width, _height;
-public:
-	Window(int width = DEFAULT_WIDTH, int Height = DEFAULT_HEIGHT);
+	Window(int width = DEFAULT_WIDTH, int height = DEFAULT_HEIGHT);
 	virtual ~Window() = default;
 
-	int getHeight() const {
-		return _height;
-	}
+	void set_title(const char * title) { SDL_SetWindowTitle(_window.get(), title); }
 
-	int getWidth() const {
-		return _width;
-	}
+	int height() const { return _height; }
+	int width() const  { return _width;  }
+
+	void main_loop();
+
+	virtual void setup() {}
+	virtual void setup_gl() {}
+	virtual void update() {}
+	virtual void render() {}
+	virtual void handle_event(const SDL_Event& event) {}
+	virtual void handle_keys(const Uint8 * keys) {}
 };
 
 #endif /* WINDOW_H_ */
